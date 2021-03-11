5be37a4ee5dfaaa9c5dccab3b6e98f7bbf560b8e
Alessio Gerace <a.gerace@inrim.it>
2021-03-11 19:06:58 +0100

    [FIX] config docker compose

--- build_and_run_dev.sh
+++ build_and_run_dev.sh
@@ -2,8 +2,9 @@
 if [ -d "$PWD/app/themes" ]; then
       git -C "$PWD/app/themes" pull
   else
-#      git -C "$PWD/app" clone git@gitlab.ininrim.it:inrimsi/microservices-libs/formio-italia-theme.git themes
       git -C "$PWD/app" clone https://gitlab.ininrim.it/inrimsi/microservices-libs/formio-italia-theme.git themes
+      #      git -C "$PWD/app" clone git@gitlab.ininrim.it:inrimsi/microservices-libs/formio-italia-theme.git themes
+
 fi
 if [ ! -f ".env-test" ]; then
     echo ".env-test File not found!"

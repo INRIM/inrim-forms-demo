8cd0113ba3c3d2e0308ea4cd0bf5a1317520cfb7
Alessio Gerace <a.gerace@inrim.it>
2021-03-12 11:04:26 +0100

    [ADD] README LICENSE AUTHORS

--- build_and_run_dev.sh
+++ build_and_run_dev.sh
@@ -2,8 +2,7 @@
 if [ -d "$PWD/app/themes" ]; then
       git -C "$PWD/app/themes" pull
   else
-      git -C "$PWD/app" clone https://gitlab.ininrim.it/inrimsi/microservices-libs/formio-italia-theme.git themes
-      #      git -C "$PWD/app" clone git@gitlab.ininrim.it:inrimsi/microservices-libs/formio-italia-theme.git themes
+      git -C "$PWD/app" clone https://github.com/INRIM/forms-theme-italia.git themes
 
 fi
 if [ ! -f ".env-test" ]; then
